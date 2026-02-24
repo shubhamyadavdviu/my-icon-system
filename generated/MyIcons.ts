5b1592dd819d57bac94eea114d8811ff8e2e3d63
Icon-Robot <robot@github.com>
2026-02-24 10:55:24 +0000

    Robot: Automatically updated font and assets [skip ci]

--- generated/MyIcons.ts
+++ generated/MyIcons.ts
@@ -1,17 +1,25 @@
 export type MyIconsId =
+  | "notifications_outlined"
+  | "notifications_filled"
   | "club_outlined"
   | "club_filled";
 
 export type MyIconsKey =
+  | "NotificationsOutlined"
+  | "NotificationsFilled"
   | "ClubOutlined"
   | "ClubFilled";
 
 export enum MyIcons {
+  NotificationsOutlined = "notifications_outlined",
+  NotificationsFilled = "notifications_filled",
   ClubOutlined = "club_outlined",
   ClubFilled = "club_filled",
 }
 
 export const MY_ICONS_CODEPOINTS: { [key in MyIcons]: string } = {
-  [MyIcons.ClubOutlined]: "61697",
-  [MyIcons.ClubFilled]: "61698",
+  [MyIcons.NotificationsOutlined]: "61697",
+  [MyIcons.NotificationsFilled]: "61698",
+  [MyIcons.ClubOutlined]: "61699",
+  [MyIcons.ClubFilled]: "61700",
 };
