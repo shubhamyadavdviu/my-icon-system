ab7c1c5e0e9c270bea1f9bb01fda61a241ee1985
Icon-Robot <robot@github.com>
2026-02-25 17:08:51 +0000

    Robot: Automatically generated font and Dart classes [skip ci]

---
+++ lib/my_icons.dart
@@ -0,0 +1,16 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+import 'package:flutter/widgets.dart';
+
+class MyIcons {
+  MyIcons._();
+
+  static const String _fontFamily = 'MyIcons';
+  static const String _fontPackage = 'my_design_system_icons';
+
+  static const IconData notifications_outlined = IconData(0xf101, fontFamily: _fontFamily, fontPackage: _fontPackage);
+  static const IconData notifications_filled = IconData(0xf102, fontFamily: _fontFamily, fontPackage: _fontPackage);
+  static const IconData club_outlined = IconData(0xf103, fontFamily: _fontFamily, fontPackage: _fontPackage);
+  static const IconData club_filled = IconData(0xf104, fontFamily: _fontFamily, fontPackage: _fontPackage);
+  static const IconData cloud_outlined = IconData(0xf105, fontFamily: _fontFamily, fontPackage: _fontPackage);
+  static const IconData cloud_filled = IconData(0xf106, fontFamily: _fontFamily, fontPackage: _fontPackage);
+}
