77b21fde75fabc5a0731315a1dae7360171404b3
Icon-Robot <robot@github.com>
2026-02-26 13:16:49 +0000

    Robot: Automatically generated font and Dart classes [skip ci]

--- lib/my_icons.dart
+++ lib/my_icons.dart
@@ -7,10 +7,12 @@ class MyIcons {
   static const String _fontFamily = 'MyIcons';
   static const String _fontPackage = 'my_design_system_icons';
 
-  static const IconData notifications_outlined = IconData(0xf101, fontFamily: _fontFamily, fontPackage: _fontPackage);
-  static const IconData notifications_filled = IconData(0xf102, fontFamily: _fontFamily, fontPackage: _fontPackage);
-  static const IconData club_outlined = IconData(0xf103, fontFamily: _fontFamily, fontPackage: _fontPackage);
-  static const IconData club_filled = IconData(0xf104, fontFamily: _fontFamily, fontPackage: _fontPackage);
-  static const IconData cloud_outlined = IconData(0xf105, fontFamily: _fontFamily, fontPackage: _fontPackage);
-  static const IconData cloud_filled = IconData(0xf106, fontFamily: _fontFamily, fontPackage: _fontPackage);
+  static const IconData percent_outlined = IconData(0xf101, fontFamily: _fontFamily, fontPackage: _fontPackage);
+  static const IconData percent_filled = IconData(0xf102, fontFamily: _fontFamily, fontPackage: _fontPackage);
+  static const IconData notifications_outlined = IconData(0xf103, fontFamily: _fontFamily, fontPackage: _fontPackage);
+  static const IconData notifications_filled = IconData(0xf104, fontFamily: _fontFamily, fontPackage: _fontPackage);
+  static const IconData club_outlined = IconData(0xf105, fontFamily: _fontFamily, fontPackage: _fontPackage);
+  static const IconData club_filled = IconData(0xf106, fontFamily: _fontFamily, fontPackage: _fontPackage);
+  static const IconData cloud_outlined = IconData(0xf107, fontFamily: _fontFamily, fontPackage: _fontPackage);
+  static const IconData cloud_filled = IconData(0xf108, fontFamily: _fontFamily, fontPackage: _fontPackage);
 }
